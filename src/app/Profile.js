5236d75fe6a72daae0344baf0dc5f12138649442
frandd07 <fran.doblado.diaz@gmail.com>
2024-10-30 10:39:56 +0100

    Profile.java

--- src/app/Profile.js
+++ src/app/Profile.js
@@ -1,10 +1,16 @@
 import { getImageUrl } from './utils2.js';
 
 function Avatar({ person, size }) {
+  let letra;
+  if(size < 90){
+    letra = "s"
+  }else if(size > 90){
+    letra = "b"
+  }
   return (
     <img
       className="avatar"
-      src={getImageUrl(person, 'b')}
+      src={getImageUrl(person,letra)}
       alt={person.name}
       width={size}
       height={size}
@@ -15,7 +21,7 @@ function Avatar({ person, size }) {
 export default function Profile() {
   return (
     <Avatar
-      size={40}
+      size={400}
       person={{ 
         name: 'Gregorio Y. Zara', 
         imageId: '7vQD0fP'
--- src/app/Profile2.js
+++ src/app/Profile2.js
@@ -1,8 +1,17 @@
+function Card({children}){
+  return(
+    <div className="card">
+          <div className="card-content">
+            {children}
+          </div>
+    </div>
+  )
+}
+
 export default function Profile2() {
     return (
       <div>
-        <div className="card">
-          <div className="card-content">
+        <Card>
             <h1>Foto</h1>
             <img
               className="avatar"
@@ -11,15 +20,15 @@ export default function Profile2() {
               width={70}
               height={70}
             />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-content">
-            <h1>Información</h1>
+          </Card>
+          <Card>
+          <h1>Información</h1>
             <p>Aklilu Lemma fue un destacado científico etíope que descubrió un tratamiento natural para la esquistosomiasis.</p>
-          </div>
-        </div>
+          </Card>
       </div>
+        
+        
+            
     );
   }
   
